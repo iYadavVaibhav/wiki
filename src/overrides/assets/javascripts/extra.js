85a2c98f55d1e2474f2931c04de2593141dcf5c8
Vaibhav Yadav <vebs0205@gmail.com>
2023-01-23 00:39:08 +0000

    fixes date and apple logo

--- src/overrides/assets/javascripts/extra.js
+++ src/overrides/assets/javascripts/extra.js
@@ -8,11 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByTagName('head')[0].appendChild(link);
 });
 
+var link = document.createElement('link');
+link.rel = "apple-touch-icon";
+link.sizes = "180x180";
+link.href = window.location.origin+"/wiki/assets/images/apple-touch-icon.png";
+document.getElementsByTagName('head')[0].appendChild(link);
 
 var link = document.createElement('link');
 link.rel = "icon";
 link.sizes = "32x32";
-link.href = "assets/images/favicon-32x32.png";
+link.href = window.location.origin+"/wiki/assets/images/favicon-32x32.png";
 document.getElementsByTagName('head')[0].appendChild(link);
 
 // <!--Favicons and icons -->
